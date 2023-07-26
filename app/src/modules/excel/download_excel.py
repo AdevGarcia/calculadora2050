460206e1d13e6c60be028de86aefe381773d843d
Angel A. Garcia Gonzalez <angelgarcia@idom.com>
2023-07-26 12:21:51 +0200

    add excel downloader

--- app/src/modules/excel/download_excel.py
+++ app/src/modules/excel/download_excel.py
@@ -19,10 +19,10 @@ router = APIRouter()
 def download(
     # current_user: models_user.User = Depends(deps.get_current_active_user)
     ) -> Any:
- 
+
     media_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
     
-    file_name = 'Calculadora_Colombiana_de_Carbono_2050_V24.xlsx'
+    file_name = 'Calculadora_Colombiana_de_Carbono_2050.xlsx'
     file_location = Path(__file__).parents[3] / f'data/{file_name}'
     
     logger.info(f'Download Excel')
