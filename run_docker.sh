ae1c1288f8251145bf7d3a32b4a849945dbe44f1
Angel A. Garcia Gonzalez <angelgarcia@idom.com>
2023-08-17 18:13:21 +0200

    change wait time

--- run_docker.sh
+++ run_docker.sh
@@ -10,25 +10,11 @@ docker-compose up -d
 echo "load data start in 40 seconds"
 sleep 30
 
-echo "10"
-sleep 1
-echo "9"
-sleep 1
-echo "8"
-sleep 1
-echo "7"
-sleep 1
-echo "6"
-sleep 1
-echo "5"
-sleep 1
-echo "4"
-sleep 1
-echo "3"
-sleep 1
-echo "2"
-sleep 1
-echo "1"
+for i in {1..10}
+do
+   echo "Wait $i/10 times"
+   sleep 1
+done
 
 echo "load data"
 export PYTHONPATH=$PWD
