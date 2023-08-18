482155038496faed7c1891d2faa61e29560a7c35
Angel A. Garcia Gonzalez <angelgarcia@idom.com>
2023-08-18 15:51:38 +0200

    cambiso menores

--- run_docker.sh
+++ run_docker.sh
@@ -4,14 +4,16 @@ set -e
 echo "killing old docker processes"
 docker-compose rm -fs
 
+# docker system prune -a
+
 echo "building docker containers"
 docker-compose up -d
 
-echo "load data start in 30 seconds"
+echo "load data start in 40 seconds"
 
 for i in {1..30}
 do
-   echo "Wait $i/30 times"
+   echo "Wait $i/40 times"
    sleep 1
 done
 
