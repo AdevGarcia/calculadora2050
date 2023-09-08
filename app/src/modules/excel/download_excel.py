6ab268cdf755a8e0677f1c1bc0956dd2fd3a835c
Angel A. Garcia Gonzalez <angelgarcia@idom.com>
2023-09-08 11:58:23 +0200

    territorios

--- app/src/modules/excel/download_excel.py
+++ app/src/modules/excel/download_excel.py
@@ -28,3 +28,47 @@ def download(
     logger.info(f'Download Excel')
   
     return FileResponse(path=file_location, media_type=media_type, filename=file_name)
+
+@router.get('/territorial/bolivar')
+def download_bolivar(
+    current_user: models_user.User = Depends(deps.get_current_active_user)
+    ) -> Any:
+
+    media_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    
+    file_name = 'Calculadora_Colombiana_de_Carbono_2050_Bolivar.xlsx'
+    file_location = Path(__file__).parents[3] / f'data/{file_name}'
+    
+    logger.info(f'Download Excel Bolivar')
+  
+    return FileResponse(path=file_location, media_type=media_type, filename=file_name)
+
+
+@router.get('/territorial/caqueta')
+def download_caqueta(
+    current_user: models_user.User = Depends(deps.get_current_active_user)
+    ) -> Any:
+
+    media_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    
+    file_name = 'Calculadora_Colombiana_de_Carbono_2050_Caqueta.xlsx'
+    file_location = Path(__file__).parents[3] / f'data/{file_name}'
+    
+    logger.info(f'Download Excel Caqueta')
+  
+    return FileResponse(path=file_location, media_type=media_type, filename=file_name)
+
+
+@router.get('/territorial/cundinamarca')
+def download_cundinamarca(
+    current_user: models_user.User = Depends(deps.get_current_active_user)
+    ) -> Any:
+
+    media_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    
+    file_name = 'Calculadora_Colombiana_de_Carbono_2050_Cundinamarca.xlsx'
+    file_location = Path(__file__).parents[3] / f'data/{file_name}'
+    
+    logger.info(f'Download Excel Cundinamarca')
+  
+    return FileResponse(path=file_location, media_type=media_type, filename=file_name)
